8743b0ca88cedbc953fce5e88f8309ffd8982008
soportewavesoluciones <163604655+soportewavesoluciones@users.noreply.github.com>
2024-07-17 23:48:54 -0300

    Update boot.py

--- chapelco/boot.py
+++ chapelco/boot.py
@@ -8,8 +8,13 @@ from ble_ConfigNetwork import BLESimplePeripheral, demo
 import machine
 import rgb
 import senko
+import _thread
+import threading
 
 rgb.change_led_color("white")
+
+# Evento para controlar el parpadeo del LED
+stop_blinking_event = threading.Event()
 
 # Función para cargar la configuración desde un archivo JSON
 def cargar_configuracion(nombre_archivo):
@@ -47,13 +52,14 @@ def connect_wlan(ssid, password):
         rgb.change_led_color("green")
     return True
 
-def led_blink():
+def led_blink(stop_event):
     """Makes the LED blink to indicate OTA update process."""
-    while True:
+    while not stop_event.is_set():
         rgb.change_led_color("blue")
         time.sleep(0.5)
         rgb.change_led_color("")
         time.sleep(0.5)
+    rgb.change_led_color("green")  # Set the LED to green after stopping the blink
 
 def main():
     """Main function. Runs after board boot, before main.py."""
@@ -73,8 +79,8 @@ def main():
     try:
         print("Checking for OTA update...")
         # Start LED blinking in a separate thread
-        import _thread
-        _thread.start_new_thread(led_blink, ())
+        blink_thread = threading.Thread(target=led_blink, args=(stop_blinking_event,))
+        blink_thread.start()
         
         if OTA.update():
             print("Updated to the latest version! Rebooting...")
@@ -84,6 +90,8 @@ def main():
     finally:
         # Stop LED blinking and set it to a stable color after OTA update check
         print("OTA finally")
+        stop_blinking_event.set()
+        blink_thread.join()  # Wait for the blink_thread to finish
         rgb.change_led_color("green")
 
 if __name__ == "__main__":
